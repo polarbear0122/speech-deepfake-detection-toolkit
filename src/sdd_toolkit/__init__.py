e28b506c04fda00684fd44fa4851381deb8a7058
polarbear0122 <122656264+polarbear0122@users.noreply.github.com>
2026-09-10 19:08:32 +0800

    feat: expose protocol helpers from package

--- src/sdd_toolkit/__init__.py
+++ src/sdd_toolkit/__init__.py
@@ -1,5 +1,12 @@
 """Core utilities for the Speech Deepfake Detection Toolkit."""
 
 from .metrics import compute_eer
+from .protocol import ProtocolEntry, labels, load_protocol, parse_protocol_line
 
-__all__ = ["compute_eer"]
+__all__ = [
+    "compute_eer",
+    "ProtocolEntry",
+    "labels",
+    "load_protocol",
+    "parse_protocol_line",
+]
